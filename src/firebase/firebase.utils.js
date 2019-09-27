509a7767db892fe1ad7938b523ce73c74b671b2b
Vladymyr Strizhov <bud.strizhov@gmail.com>
2019-09-27 09:53:24 +0300

    udemy add dispatch and correct dtat in firebase.utile.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,18 +1,43 @@
 import firebase from 'firebase/app';
-import "firebase/firestore";
-import "firebase/auth";
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
   apiKey: "AIzaSyBoxmSP4Y7MG4GbVRYlno9FX1i1hEnUT4k",
   authDomain: "udm-crwn-proj1104.firebaseapp.com",
   databaseURL: "https://udm-crwn-proj1104.firebaseio.com",
   projectId: "udm-crwn-proj1104",
-  storageBucket: "",
+  storageBucket: "udm-crwn-proj1104.appspot.com",
   messagingSenderId: "347327394957",
   appId: "1:347327394957:web:98db4dbb956629bd249ec4"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
+import SHOP_DATA from './shop.data.js';
+
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-import SHOP_DATA from "./shop.data";
-
 class ShopPage extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -14,20 +13,16 @@ class ShopPage extends React.Component {
     };
   }
 
-  render(){
-    const {collections} = this.state;
-    console.log(collections);
-    return(
+  render() {
+    const { collections } = this.state;
+    return (
       <div className='shop-page'>
-        {
-          collections.map(({id,items,title}) =>(
-            <CollectionPreview key={id} items={items} title={title} />            
-          ))
-        }
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
     );
   }
-  
 }
 
 export default ShopPage;
