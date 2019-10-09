557f247b9dc1e3919a2c1cd1456efdc398e9e91c
Vladymyr Strizhov <bud.strizhov@gmail.com>
2019-10-09 21:05:21 +0300

    added createStructuredSelector and have made all code mmemoized with selectors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {createStructuredSelector} from 'reselect';
 
 import './App.css';
 
@@ -9,7 +10,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-
+import {selectCurrentUser} from './redux/user/user.selectors';
 import { setCurrentUser } from "./redux/user/user.actions";
 
 
@@ -62,12 +63,12 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
-})
+const mapStateToProps = createStructuredSelector ({
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps )(App);
